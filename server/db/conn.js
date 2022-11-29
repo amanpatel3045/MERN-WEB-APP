de23ba065e0b1f4f7702edae92ead0b88a017453
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2022-11-29 23:04:10 +0530

    Add files via upload

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,14 +1,24 @@
 const mongoose = require("mongoose");
 
 const DB = process.env.DATABASE;
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
-  .then(() => {
-    console.log(`connection successfull`);
-  })
-  .catch((err) => console.log(`no connection`));
+// const DB_PASS = process.env.DB_PASS
+console.log(DB, " URL CHECK ");
+async function connection() {
+  await mongoose.connect(DB);
+  console.log("connect bd ");
+  // await mongoose.connect('mongodb://127.0.0.1:27017/address')
+}
+
+// export default connection
+module.exports = { connection };
+
+// mongoose.connect(DB, {
+//     useNewUrlParser: true,
+//     // useCreateIndex: true,
+//     useUnifiedTopology: true,
+//     // useFindAndModify: false,
+//   })
+//   .then(() => {
+//     console.log(`connection successfull`);
+//   })
+//   .catch((err) => console.log(`no connection ${err}`));
